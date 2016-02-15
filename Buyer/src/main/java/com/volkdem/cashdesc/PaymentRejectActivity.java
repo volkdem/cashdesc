0b0c828a80621bffa49de334163dbee1f85665d2
es.volkov <es.volkov@yandex.ru>
2016-02-15 08:58:12 +0300

    CashDesc init project

---
+++ Buyer/src/main/java/com/volkdem/cashdesc/PaymentRejectActivity.java
@@ -0,0 +1,13 @@
+package com.volkdem.cashdesc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentRejectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_reject);
+    }
+}
---
+++ Buyer/src/main/java/com/volkdem/cashdesc/ScanShopCodeActivity.java
@@ -0,0 +1,13 @@
+package com.volkdem.cashdesc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScanShopCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_shop_code);
+    }
+}
