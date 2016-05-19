7f1389c5c9fd253daafe49b776b53a1a3137e450
es.volkov <es.volkov@yandex.ru>
2016-05-19 10:46:34 +0300

    Search Widget has been added

--- Officer/src/main/java/officer/cashdesc/volkdem/com/officer/SearchOrdersActivity.java
+++ Officer/src/main/java/officer/cashdesc/volkdem/com/officer/SearchOrdersActivity.java
@@ -1,7 +1,11 @@
 package officer.cashdesc.volkdem.com.officer;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 
@@ -11,12 +15,30 @@ public class SearchOrdersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_orders);
+
+        handleIntent( getIntent() );
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent( intent );
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate( R.menu.search_orders_options_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem( R.id.search ).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo( getComponentName() ) );
+
+        return true;
+    }
+
+    private void handleIntent(Intent intent ) {
+        if (Intent.ACTION_SEARCH.equals( intent.getAction() ) ) {
+            String query = intent.getStringExtra( SearchManager.QUERY );
+        }
     }
 }
